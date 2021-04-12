136a4859a52c3990c3e784476451f3546539dc73
Adelmo Menezes <adelmo.menezes2010@gmail.com>
2021-04-12 13:43:15 -0300

    efeito nos componentes

--- static/js/partis/script.js
+++ static/js/partis/script.js
@@ -1,100 +1,115 @@
-//Animação menu responsivo
-$('.btn-menu, .btn-close').on('click', function () {
-  $('.menu').slideToggle('slow');
-});
+$(document).ready(function () {
+  //Animação menu responsivo
+  $('.btn-menu, .btn-close').on('click', function () {
+    $('.menu').slideToggle('slow');
+  });
 
-$('nav ul li').click(function () {
-  $('.menu').hide();
-});
+  $('nav ul li').click(function () {
+    $('.menu').hide();
+  });
 
-//Scroll menu
-$('nav a[href^="#"]').on('click', function (e) {
-  e.preventDefault();
-  var id = $(this).attr('href'),
+  //Scroll menu
+  $('nav a[href^="#"]').on('click', function (e) {
+    e.preventDefault();
+    var id = $(this).attr('href'),
       targetOffset = $(id).offset().top;
 
-  $('html, body').animate(
+    $('html, body').animate(
       {
-          scrollTop: targetOffset,
+        scrollTop: targetOffset,
       },
       1000
-  );
-});
+    );
+  });
 
-//Scroll top
-$(document).ready(function () {
+  //Scroll top
+
   var scrollTop = $('.scrollTop');
 
   $(window).scroll(function () {
-      var topPos = $(this).scrollTop();
+    var topPos = $(this).scrollTop();
 
-      //quantidade Y que o botão de scroll vai aparecer
-      if (topPos > 500) {
-          $(scrollTop).css('opacity', '1');
-      } else {
-          $(scrollTop).css('opacity', '0');
-      }
+    //quantidade Y que o botão de scroll vai aparecer
+    if (topPos > 500) {
+      $(scrollTop).css('opacity', '1');
+    } else {
+      $(scrollTop).css('opacity', '0');
+    }
   });
   //velocidade a animação
   $(scrollTop).click(function () {
-      $('html, body').animate(
-          {
-              scrollTop: 0,
-          },
-          1300
-      );
-      return false;
+    $('html, body').animate(
+      {
+        scrollTop: 0,
+      },
+      1300
+    );
+    return false;
   });
-});
 
-//Dark Mode
-var darkMode;
+  //Dark Mode
+  var darkMode;
 
-if (localStorage.getItem('dark-mode')) {
-  darkMode = 'light';
-} else {
-  darkMode = localStorage.getItem('dark-mode');
-}
+  if (localStorage.getItem('dark-mode')) {
+    darkMode = 'light';
+  } else {
+    darkMode = localStorage.getItem('dark-mode');
+  }
 
-localStorage.getItem('dark-mode', darkMode);
+  localStorage.getItem('dark-mode', darkMode);
 
-if (localStorage.getItem('dark-mode') == 'dark') {
-  $('body').addClass('dark');
-  $('.dark-button').hide();
-  $('.light-button').show();
-}
+  if (localStorage.getItem('dark-mode') == 'dark') {
+    $('body').addClass('dark');
+    $('.dark-button').hide();
+    $('.light-button').show();
+  }
 
 
-$('.dark-button').on('click', function () {
-  $('.dark-button').hide();
-  $('.light-button').show();
-  $('body').addClass('dark');
-  localStorage.setItem('dark-mode', 'dark');
-});
-
-$('.light-button').on('click', function () {
-  $('.light-button').hide();
-  $('.dark-button').show();
-  $('body').removeClass('dark');
-  localStorage.setItem('dark-mode', 'light');
-});
-
-//Contato
-valor = document.getElementById('campo').value
-
-function animateLinks(){
-  this.navLinks.forEach((link, index) => {  
-  link.style.animation
-  ? (link.style.animation = "")
-  : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`)
+  $('.dark-button').on('click', function () {
+    $('.dark-button').hide();
+    $('.light-button').show();
+    $('body').addClass('dark');
+    localStorage.setItem('dark-mode', 'dark');
   });
-}
 
-ScrollReveal().reveal('.headline', {
-  delay: 375,
-  duration: 500,
-  reset: true
-});
+  $('.light-button').on('click', function () {
+    $('.light-button').hide();
+    $('.dark-button').show();
+    $('body').removeClass('dark');
+    localStorage.setItem('dark-mode', 'light');
+  });
 
 
+  //Revelar animação
+  const sr = ScrollReveal({
+    origin: 'top',
+    distance: '50px',
+    duration: 2000,
+    reset: true
+  });
 
+  //Home delay
+  sr.reveal('.button', { delay: 100 })
+
+  //Slide
+  sr.reveal('.daley-0', { delay: 0 })
+  sr.reveal('.daley-1', { delay: 100 })
+  sr.reveal('.daley-2', { delay: 200 })
+  sr.reveal('.daley-3', { delay: 300 })
+  sr.reveal('.daley-4', { delay: 400 })
+  sr.reveal('.daley-5', { delay: 500 })
+
+  const rs = ScrollReveal({
+    origin: 'left',
+    distance: '50px',
+    duration: 2000,
+    reset: true
+  });
+
+  rs.reveal('.daley-l1', { delay: 300 })
+  rs.reveal('.daley-l2', { delay: 300 })
+  rs.reveal('.daley-l3', { delay: 300 })
+  rs.reveal('.daley-l4', { delay: 400 })
+
+
+})
