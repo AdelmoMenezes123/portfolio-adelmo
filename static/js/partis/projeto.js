8cb984ed2a3e8968327e191c54ae75f6ccba8c31
Adelmo Menezes <adelmo@connectlab.com.br>
2022-10-13 15:53:21 -0300

    add projeto

--- static/js/partis/projeto.js
+++ static/js/partis/projeto.js
@@ -1,227 +1,239 @@
-
-const dados = [{
+const dados = [
+  {
     id: 0,
-    title: 'Z-FLIX',
-    img: 'static/img/projetos/zflix.png',
+    title: "Z-FLIX",
+    img: "static/img/projetos/zflix.png",
     info: `Clone da Netflix, consumindo API do site TheMoviedatabase.org, Projeto feito a partir de uma live no youtube ministrada pelo canal: Bonieky Lacerda. `,
-    link1: '/static/img/techs/reactjs.svg',
-    link2: '/static/img/techs/css.svg',
-    link3: '/static/img/techs/sass.svg',
-    link_projeto: 'https://z-flix.vercel.app/',
-    github: 'https://github.com/AdelmoMenezes123/z-flix/blob/master/README.md'
-
-},
-{
+    link1: "/static/img/techs/reactjs.svg",
+    link2: "/static/img/techs/css.svg",
+    link3: "/static/img/techs/sass.svg",
+    link_projeto: "https://z-flix.vercel.app/",
+    github: "https://github.com/AdelmoMenezes123/z-flix/blob/master/README.md",
+  },
+  {
     id: 1,
-    title: 'Get-Mecânico TCC',
-    img: '/static/img/projetos/get-mecanico.jpeg',
+    title: "Get-Mecânico TCC",
+    img: "/static/img/projetos/get-mecanico.jpeg",
     info: `TCC. Trabalho de Conclusão de Curso. 
     Você pode se cadastrar como usuario, pesquisar mecânicos e solicitar orçamentos.
     Ou você pode se cadastrar como mecânico e aceita as chamadas de servios de um usuario.`,
-    link1: '/static/img/handle.png',
-    link2: 'https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/nodejs/nodejs.png',
-    link3: '/static/img/techs/typescript.png',
-    link_projeto: 'http://getmecanico.herokuapp.com//',
-    github: 'https://github.com/AdelmoMenezes123/getmecanico/blob/master/README.md'
-
-},
-{
+    link1: "/static/img/handle.png",
+    link2:
+      "https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/nodejs/nodejs.png",
+    link3: "/static/img/techs/typescript.png",
+    link_projeto: "http://getmecanico.herokuapp.com//",
+    github:
+      "https://github.com/AdelmoMenezes123/getmecanico/blob/master/README.md",
+  },
+  {
     id: 2,
-    title: 'User Manager',
-    img: '/static/img/projetos/user-manager.png',
-    info: 'Projeto desenvolvido no curso online de JavaScript da Hcode Treinamentos.',
-    link1: '/static/img/techs/html.png',
-    link2: '/static/img/techs/js.svg',
-    link3: '/static/img/techs/css.svg',
-    link_projeto: 'https://user-manager-six.vercel.app/',
-    github: 'https://github.com/AdelmoMenezes123/user-manager/blob/master/README.md'
-
-},
-{
+    title: "User Manager",
+    img: "/static/img/projetos/user-manager.png",
+    info: "Projeto desenvolvido no curso online de JavaScript da Hcode Treinamentos.",
+    link1: "/static/img/techs/html.png",
+    link2: "/static/img/techs/js.svg",
+    link3: "/static/img/techs/css.svg",
+    link_projeto: "https://user-manager-six.vercel.app/",
+    github:
+      "https://github.com/AdelmoMenezes123/user-manager/blob/master/README.md",
+  },
+  {
     id: 3,
-    title: 'DesenFlix',
-    img: 'static/img/projetos/desenflix.png',
-    info: 'Desenflix, desenhos, series, animes e filmes, Projeto pessoal criado para aperfeiçoar meus conhecimentos em tecnologias como:',
-    link1: '/static/img/techs/html.svg',
-    link2: '/static/img/techs/css.svg',
-    link3: '/static/img/techs/js.svg',
-    link_projeto: 'https://desen-flix.vercel.app/',
-    github: 'https://github.com/AdelmoMenezes123/DesenFlix/blob/master/README.md'
-
-},
-{
+    title: "DesenFlix",
+    img: "static/img/projetos/desenflix.png",
+    info: "Desenflix, desenhos, series, animes e filmes, Projeto pessoal criado para aperfeiçoar meus conhecimentos em tecnologias como:",
+    link1: "/static/img/techs/html.svg",
+    link2: "/static/img/techs/css.svg",
+    link3: "/static/img/techs/js.svg",
+    link_projeto: "https://desen-flix.vercel.app/",
+    github:
+      "https://github.com/AdelmoMenezes123/DesenFlix/blob/master/README.md",
+  },
+  {
     id: 4,
-    title: 'Proffy-rocktseat',
-    img: 'static/img/projetos/proffy.png',
-    info: 'Semana Next Level Week 2, OmniStack, envento promovido pela Rocketseat. Foi usado tecnologias como:',
-    link1: '/static/img/techs/html.svg',
-    link2: '/static/img/techs/css.svg',
-    link3: '/static/img/techs/js.svg',
-    link_projeto: 'https://proffy-rocktseat.vercel.app/',
-    github: 'https://github.com/AdelmoMenezes123/Proffy-Rocktseat/blob/master/README.md'
-},
-{
+    title: "Proffy-rocktseat",
+    img: "static/img/projetos/proffy.png",
+    info: "Semana Next Level Week 2, OmniStack, envento promovido pela Rocketseat. Foi usado tecnologias como:",
+    link1: "/static/img/techs/html.svg",
+    link2: "/static/img/techs/css.svg",
+    link3: "/static/img/techs/js.svg",
+    link_projeto: "https://proffy-rocktseat.vercel.app/",
+    github:
+      "https://github.com/AdelmoMenezes123/Proffy-Rocktseat/blob/master/README.md",
+  },
+  {
     id: 5,
-    title: 'Move Body',
-    img: 'static/img/projetos/movebody2.png',
-    info: 'Semana Next Level Week 4, OmniStack, envento promovido pela Rocketseat. Foi usado tecnologias como:',
-    link1: '/static/img/techs/reactjs.svg',
-    link2: '/static/img/techs/css.svg',
-    link3: '/static/img/techs/js.svg',
-    link_projeto: 'https://movebody.vercel.app/',
-    github: 'https://github.com/AdelmoMenezes123/movebody/blob/master/README.md'
-},
-{
+    title: "Move Body",
+    img: "static/img/projetos/movebody2.png",
+    info: "Semana Next Level Week 4, OmniStack, envento promovido pela Rocketseat. Foi usado tecnologias como:",
+    link1: "/static/img/techs/reactjs.svg",
+    link2: "/static/img/techs/css.svg",
+    link3: "/static/img/techs/js.svg",
+    link_projeto: "https://movebody.vercel.app/",
+    github:
+      "https://github.com/AdelmoMenezes123/movebody/blob/master/README.md",
+  },
+  {
     id: 6,
-    title: 'Nerd-Folio',
-    img: 'static/img/projetos/nerdfolio.jpeg',
+    title: "Nerd-Folio",
+    img: "static/img/projetos/nerdfolio.jpeg",
     info: 'Bloco de notas Pessoal para você armazenar status de atividades recente, no mundo "NERD"',
-    link1: 'https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/mongodb/mongodb.png',
-    link2: '/static/img/handle.png',
-    link3: 'https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/nodejs/nodejs.png',
-    link_projeto: 'https://nerdfolio.herokuapp.com/',
-    github: 'https://github.com/AdelmoMenezes123/note-app/blob/master/README.md'
-
-},
-{
+    link1:
+      "https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/mongodb/mongodb.png",
+    link2: "/static/img/handle.png",
+    link3:
+      "https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/nodejs/nodejs.png",
+    link_projeto: "https://nerdfolio.herokuapp.com/",
+    github:
+      "https://github.com/AdelmoMenezes123/note-app/blob/master/README.md",
+  },
+  {
     id: 7,
-    title: 'Calculadora VPL',
-    img: 'static/img/projetos/calcula-vpl.jpeg',
-    info: ' Trabalho de faculdade para a materia - Gest. Financeira - Calculadora VPL',
-    link1: '/static/img/techs/reactjs.svg',
-    link2: '/static/img/techs/material.svg',
-    link3: '/static/img/techs/css.svg',
-    link_projeto: 'https://trabalho-calculadora-vpl.vercel.app/',
-    github: 'https://github.com/AdelmoMenezes123/trabalho-calculadora-vpl/blob/master/README.md'
-
-},
-{
+    title: "Calculadora VPL",
+    img: "static/img/projetos/calcula-vpl.jpeg",
+    info: " Trabalho de faculdade para a materia - Gest. Financeira - Calculadora VPL",
+    link1: "/static/img/techs/reactjs.svg",
+    link2: "/static/img/techs/material.svg",
+    link3: "/static/img/techs/css.svg",
+    link_projeto: "https://trabalho-calculadora-vpl.vercel.app/",
+    github:
+      "https://github.com/AdelmoMenezes123/trabalho-calculadora-vpl/blob/master/README.md",
+  },
+  {
     id: 8,
-    title: 'IMC com react',
-    img: 'static/img/projetos/imc-react.png',
-    info: 'Projeto de estudo, para aperfeicoar meus conhecimentos em react',
-    link1: '/static/img/techs/reactjs.svg',
-    link2: '/static/img/techs/material.svg',
-    link3: '/static/img/techs/sass.svg',
-    link_projeto: 'https://imc-three.vercel.app/',
-    github: 'https://github.com/AdelmoMenezes123/imc/blob/master/README.md',
-
-},
-{
+    title: "IMC com react",
+    img: "static/img/projetos/imc-react.png",
+    info: "Projeto de estudo, para aperfeiçoar meus conhecimentos em react",
+    link1: "/static/img/techs/reactjs.svg",
+    link2: "/static/img/techs/material.svg",
+    link3: "/static/img/techs/sass.svg",
+    link_projeto: "https://imc-three.vercel.app/",
+    github: "https://github.com/AdelmoMenezes123/imc/blob/master/README.md",
+  },
+  {
     id: 9,
-    title: 'Pizzaria',
-    img: 'static/img/projetos/pizza.jpeg',
-    info: ' Projeto feito a partir de uma live no youtube ministrada pelo canal: Bonieky Lacerda.',
-    link1: '/static/img/techs/html.svg',
-    link2: '/static/img/techs/css.svg',
-    link3: '/static/img/techs/js.svg',
-    link_projeto: 'https://pizzaria-eight.vercel.app/',
-    github: 'https://github.com/AdelmoMenezes123/pizzaria/blob/master/README.md'
-
-},
-{
+    title: "Pizzaria",
+    img: "static/img/projetos/pizza.jpeg",
+    info: " Projeto feito a partir de uma live no youtube ministrada pelo canal: Bonieky Lacerda.",
+    link1: "/static/img/techs/html.svg",
+    link2: "/static/img/techs/css.svg",
+    link3: "/static/img/techs/js.svg",
+    link_projeto: "https://pizzaria-eight.vercel.app/",
+    github:
+      "https://github.com/AdelmoMenezes123/pizzaria/blob/master/README.md",
+  },
+  {
     id: 10,
-    title: 'Expense Control',
-    img: 'static/img/projetos/expense-control.png',
-		info: `Sistema criado para controlar suas transações, 
+    title: "Expense Control",
+    img: "static/img/projetos/expense-control.png",
+    info: `Sistema criado para controlar suas transações, 
 						onde você coloca sua receita e dispesa e o sistema calcula os valores.`,
-    link1: '/static/img/techs/html.svg',
-    link2: '/static/img/techs/css.svg',
-    link3: '/static/img/techs/js.svg',
-    link_projeto: 'https://expense-control-nu.vercel.app/',
-    github: 'https://github.com/AdelmoMenezes123/expense-control',
-
-},
-{
+    link1: "/static/img/techs/html.svg",
+    link2: "/static/img/techs/css.svg",
+    link3: "/static/img/techs/js.svg",
+    link_projeto: "https://expense-control-nu.vercel.app/",
+    github: "https://github.com/AdelmoMenezes123/expense-control",
+  },
+  {
     id: 11,
-    title: 'Jogo da velha',
-    img: 'static/img/projetos/jogo-da-velha.png',
+    title: "Repous Github",
+    img: "static/img/projetos/repous-github.png",
+    info: `Projeto de estudo, para aperfeiçoar meus conhecimentos em React/styled-components`,
+    link1: "/static/img/techs/reactjs.svg",
+    link2: "/static/img/techs/css.svg",
+    link3: "/static/img/techs/js.svg",
+    link_projeto: "https://repous-github.vercel.app/",
+    github:
+      "https://github.com/AdelmoMenezes123/repous-github/blob/master/README.md",
+  },
+  {
+    id: 12,
+    title: "Templete-clone-Awax",
+    img: "static/img/projetos/templete-awax.png",
+    info: "Projeto de estudo, para aperfeiçoar meus conhecimentos em CSS/Flex-box",
+    link1: "/static/img/techs/html.svg",
+    link2: "/static/img/techs/css.svg",
+    link3: "/static/img/techs/js.svg",
+    link_projeto: "https://templete-awax.vercel.app/",
+    github:
+      "https://github.com/AdelmoMenezes123/templete-awax/blob/master/README.md",
+  },
+  {
+    id: 13,
+    title: "Templete-Flexbox",
+    img: "static/img/projetos/templete-flexbox.png",
+    info: `Projeto de estudo, para aperfeiçoar meus conhecimentos em CSS/Flex-box`,
+    link1: "/static/img/techs/html.svg",
+    link2: "/static/img/techs/css.svg",
+    link3: "/static/img/techs/js.svg",
+    link_projeto: "https://templete-flex.vercel.app/",
+    github:
+      "https://github.com/AdelmoMenezes123/templete-flex/blob/master/README.md",
+  },
+  {
+    id: 14,
+    title: "Template Getmecanico",
+    img: "static/img/projetos/template-get-mecanico.png",
+    info: `Projeto de estudo, para aperfeiçoar meus conhecimentos em CSS/Flex-box`,
+    link1: "/static/img/techs/html.svg",
+    link2: "/static/img/techs/css.svg",
+    link3: "/static/img/techs/js.svg",
+    link_projeto: "https://template-getmecanico.vercel.app/",
+    github:
+      "https://github.com/AdelmoMenezes123/template-getmecanico/blob/master/README.md",
+  },
+  {
+    id: 15,
+    title: "Calculadora",
+    img: "static/img/projetos/calculadora.jpeg",
+    info: `Calculadora desenvolvida como exemplo
+						do Curso Completo de JavaScript na Udemy.com ministrada pela equipe Hcode Treinamento`,
+    link1: "/static/img/techs/html.svg",
+    link2: "/static/img/techs/css.svg",
+    link3: "/static/img/techs/js.svg",
+    link_projeto: "https://calculadora-plum.vercel.app/",
+    github:
+      "https://github.com/AdelmoMenezes123/calculadora/blob/master/README.md",
+  },
+  {
+    id: 16,
+    title: "Jogo da velha",
+    img: "static/img/projetos/jogo-da-velha.png",
     info: `Projeto faculdade, feito para matéria de Processos Decisórios e Sistemas de Apoio a Decisão.
     Você pode bloquear alguns campos, pode jogar aleatório, e para pontuar,
     você tem que acerta duas sequências de 3 casas`,
-    link1: '/static/img/techs/html.svg',
-    link2: '/static/img/techs/css.svg',
-    link3: '/static/img/techs/js.svg',
-    link_projeto: 'https://jogo-da-velha-sexta.vercel.app/',
-    github: 'https://github.com/AdelmoMenezes123/jogo-da-velha-sexta/blob/master/README.md',
+    link1: "/static/img/techs/html.svg",
+    link2: "/static/img/techs/css.svg",
+    link3: "/static/img/techs/js.svg",
+    link_projeto: "https://jogo-da-velha-sexta.vercel.app/",
+    github:
+      "https://github.com/AdelmoMenezes123/jogo-da-velha-sexta/blob/master/README.md",
+  },
+];
 
-},
-{
-    id: 12,
-    title: 'Templete-clone-Awax',
-    img: 'static/img/projetos/templete-awax.png',
-    info: 'Projeto de estudo, para aperfeicoar meus conhecimentos em CSS/Flex-box',
-    link1: '/static/img/techs/html.svg',
-    link2: '/static/img/techs/css.svg',
-    link3: '/static/img/techs/js.svg',
-    link_projeto: 'https://templete-awax.vercel.app/',
-    github: 'https://github.com/AdelmoMenezes123/templete-awax/blob/master/README.md'
-
-},
-{
-    id: 13,
-    title: 'Templete-Flexbox',
-    img: 'static/img/projetos/templete-flexbox.png',
-    info: `Projeto de estudo, para aperfeicoar meus conhecimentos em CSS/Flex-box`,
-    link1: '/static/img/techs/html.svg',
-    link2: '/static/img/techs/css.svg',
-    link3: '/static/img/techs/js.svg',
-    link_projeto: 'https://templete-flex.vercel.app/',
-    github: 'https://github.com/AdelmoMenezes123/templete-flex/blob/master/README.md'
-
-},
-{
-    id: 14,
-    title: 'Template Getmecanico',
-    img: 'static/img/projetos/template-get-mecanico.png',
-    info: `Projeto de estudo, para aperfeicoar meus conhecimentos em CSS/Flex-box`,
-    link1: '/static/img/techs/html.svg',
-    link2: '/static/img/techs/css.svg',
-    link3: '/static/img/techs/js.svg',
-    link_projeto: 'https://template-getmecanico.vercel.app/',
-    github: 'https://github.com/AdelmoMenezes123/template-getmecanico/blob/master/README.md'
-
-	},
-	{
-		id: 15,
-		title: 'Calculadora',
-		img: 'static/img/projetos/calculadora.jpeg',
-		info: `Calculadora desenvolvida como exemplo
-						do Curso Completo de JavaScript na Udemy.com ministrada pela equipe Hcode Treinamento`,
-		link1: '/static/img/techs/html.svg',
-		link2: '/static/img/techs/css.svg',
-		link3: '/static/img/techs/js.svg',
-		link_projeto: 'https://calculadora-plum.vercel.app/',
-		github: 'https://github.com/AdelmoMenezes123/calculadora/blob/master/README.md',
-
-	},
-]
-
-const tech = document.querySelector('.tech')
-const img1 = document.createElement('img')
-const img2 = document.createElement('img')
-const img3 = document.createElement('img')
+const tech = document.querySelector(".tech");
+const img1 = document.createElement("img");
+const img2 = document.createElement("img");
+const img3 = document.createElement("img");
 
 const view = () => {
-    dados.map((date, index) => {
+  dados.map((date, index) => {
+    $("#fotoProjeto").attr("src", date.img);
+    $("#tituloProjeto").text(date.title);
+    $("#descricaoProjeto").text(date.info);
+    $("#gitProjeto").attr("href", date.github);
+    $("#linkProjeto").attr("href", date.link_projeto);
 
-        $('#fotoProjeto').attr('src', date.img)
-        $('#tituloProjeto').text(date.title);
-        $('#descricaoProjeto').text(date.info);
-        $('#gitProjeto').attr('href', date.github)
-        $('#linkProjeto').attr('href', date.link_projeto)
+    img1.src = date.link1;
+    img2.src = date.link2;
+    img3.src = date.link3;
+    tech.appendChild(img1);
+    tech.appendChild(img2);
+    tech.appendChild(img3);
 
-        img1.src = date.link1
-        img2.src = date.link2
-        img3.src = date.link3
-        tech.appendChild(img1)
-        tech.appendChild(img2)
-        tech.appendChild(img3)
-       
-        $('#proj').clone().appendTo('#projetos article');
-    })
-    $('#proj').remove();
-}
-
+    $("#proj").clone().appendTo("#projetos article");
+  });
+  $("#proj").remove();
+};
 
 view();
